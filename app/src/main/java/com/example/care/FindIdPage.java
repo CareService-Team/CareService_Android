753649673f4a7a3d0e4d55646a15ad379c7029da
hyunsooSong <84562885+myhuon@users.noreply.github.com>
2021-08-24 17:25:28 +0900

    버튼 클릭 시 단순 페이지 연결 코드 추가

---
+++ app/src/main/java/com/example/care/FindIdPage.java
@@ -0,0 +1,14 @@
+package com.example.care;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindIdPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_id_page);
+    }
+}
---
+++ app/src/main/java/com/example/care/WelfareWorkerPage.java
@@ -0,0 +1,14 @@
+package com.example.care;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelfareWorkerPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welfare_worker_page);
+    }
+}
