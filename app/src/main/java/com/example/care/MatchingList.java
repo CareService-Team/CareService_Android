60fd2dbe826ed1612f08ec333237c0e216cd1e3d
hyunsooSong <84562885+myhuon@users.noreply.github.com>
2021-08-27 17:15:09 +0900

    findIdPage, findPwdPage, idPrintPage, matchingListPage, matchinglistLayout 구현

--- app/src/main/java/com/example/care/MatchingList.java
+++ app/src/main/java/com/example/care/MatchingList.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MappingList extends AppCompatActivity {
+public class MatchingList extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mapping_list);
+        setContentView(R.layout.activity_matching_list);
     }
 }
